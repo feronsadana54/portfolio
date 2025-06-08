5081b427acfc192e354d96062db429da51ecedca
feronsadana54 <feronsadana16@gmail.com>
2025-06-08 10:10:58 +0700

    update: fitur form submit

--- src/App.jsx
+++ src/App.jsx
@@ -134,6 +134,34 @@ function App() {
             ))}
         </div>
       </div>
+
+      {/* kontak */}
+            <div className="kontak mt-32 p-10">
+              <h1 className="text-4xl mb-2 font-bold text-center">
+                Kontak
+              </h1>
+              <p className="text-base/loose text-center mb-10 opacity-50"></p>
+              <form action="https://formsubmit.co/feronsadana16@gmail.com" method="POST" className="bg-zinc-800 p-10 w-fit mx-auto rounded-md" autoComplete="off">
+                <div className="flex flex-col gap-6">
+                  <div className="flex flex-col gap-2">
+                    <label className="font-semibold">Nama Lengkap</label>
+                    <input type="text" name="nama" className="border border-zinc-500 p-2 rounded-md" placeholder="Masukkan Nama..." required/>
+                  </div>
+                  <div className="flex flex-col gap-2">
+                    <label className="font-semibold" >Email</label>
+                    <input type="email" name="email" className="border border-zinc-500 p-2 rounded-md" placeholder="Masukkan Email..." required/>
+                  </div>
+                  <div className="flex flex-col gap-2">
+                    <label className="font-semibold" htmlFor="pesan">Pesan</label>
+                    <textarea name="pesan" id="pesan" className="border border-zinc-500 p-2 rounded-md" placeholder="Pesan..." cols="45" rows="10" required ></textarea>
+                  </div>
+                  <div className="text-center ">
+                    <button className="bg-violet-700 p-3 rounded-lg w-f border cursor-pointer border-zinc-600 hover:bg-violet-600" type="submit">Kirim Pesan</button>
+                  </div>
+                </div>
+              </form>
+            </div>
+
     </>
   );
 }
